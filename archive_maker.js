3b365cf4290581d6c85e7fc0b7508bef576db5ea
Anton Stalnuhhin <anton.stalnuhhin@gmail.com>
2020-05-03 11:04:25 +0300

    months 03 and 04 archived

---
+++ archive_maker.js
@@ -0,0 +1,58 @@
+const MONTH = 5; // month number starts from 1, not 0.
+const DATAFILES = []
+
+const app = new Vue({
+    el: '#app',
+    created() {
+        this.load();
+    },
+    data() {
+        return {
+            games: []
+        }
+    },
+    methods: {
+        load: function () {
+            moment.locale("ru");
+            const now = moment();
+            const dates = [];
+            const data = {};
+
+            const promises = DATAFILES.map(file =>
+                fetch("data/" + file)
+                    .then(r => r.json()));
+            Promise.all(promises).then(r => {
+                const db = r.flat(2);
+                db.forEach(org => {
+                    org.games = org.games ? org.games : [];
+                    org.games.forEach(game => {
+                        game.time = moment(game.time);
+                        const tem = game.template ? org.templates[game.template] : {};
+                        game.name = game.name || tem.name || org.name;
+                        game.duration = game.duration || tem.duration || org.duration;
+                        game.image = game.image || tem.image || org.image;
+                        game.orgId = org.id;
+                        game.org = game.org || tem.org || org.org;
+                        game.chgk = game.chgk || tem.chgk || org.chgk;
+                        game.lang = game.lang || tem.lang || org.lang;
+                        game.noregistration = game.noregistration || tem.noregistration || org.noregistration;
+                        game.url = game.url || [];
+                        game.url = org.url ? game.url.concat(org.url) : game.url;
+                        game.url = tem.url ? game.url.concat(tem.url) : game.url;
+                        game.free = game.free || tem.free || org.free;
+                        game.donate = game.donate || tem.donate || org.donate;
+                        game.price = game.price || tem.price || org.price;
+                        game.desc = game.desc || tem.desc || org.desc;
+                        game.info = game.info || tem.info || org.info;
+                        game.payment = game.payment || tem.payment || org.payment;
+
+                        if (parseInt(game.time.format('M')) == MONTH) {
+                            this.games.push(game);
+                        }
+                    });
+                });
+                this.games.sort((a, b) => a.time - b.time);
+            });
+        }
+    }
+});
